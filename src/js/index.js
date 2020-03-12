cb2f8d43714090bc22351fea0afe7cf8878fc93b
willconstable <will_constable@msn.com>
2020-03-12 21:35:08 +0000

    Modules set up, search function created and display search in ui created

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,39 @@
-// Global app controller
-import x from "./test";
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-const num = 23;
+/**Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-console.log(`I imported ${x} from another module called something! variable is ${num}`);
+const controlSearch = async () => {
+    // 1 - Get query from the view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2 - New search object and add to state
+        state.search = new Search(query);
+
+        // 3 - Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4 - Search for recipes
+        await state.search.getResults();
+
+        // 5 - Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    controlSearch();
+});
+
+search.getResults();
